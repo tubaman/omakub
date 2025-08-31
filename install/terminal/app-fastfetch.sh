ef986bf7a81d7173bbc7559ef3510aaf0b2c4c4f
Ryan Nowakowski <tubaman@fattuba.com>
2025-08-30 22:08:41 -0500

    use fastfetch from debian apt repo

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Display system information in the terminal
-cd /tmp
-FASTFETCH_VERSION="2.37.0" # Use known good version
-wget -qO fastfetch.deb "https://github.com/fastfetch-cli/fastfetch/releases/download/$FASTFETCH_VERSION/fastfetch-linux-amd64.deb"
-sudo apt install -y ./fastfetch.deb
-rm fastfetch.deb
-cd -
+sudo apt install -y fastfetch
 
 # Only attempt to set configuration if fastfetch is not already set
 if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
